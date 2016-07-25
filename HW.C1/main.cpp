e70bc8c582c141b485dc6795afe92a3a7c19e9c7
Seokmin <seokmin@nhnnext.org>
2016-07-25 17:05:34 +0900

    rod cutting

---
+++ HW.C1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main(void)
+{
+
+
+	return 0;
+}
+
+int MaxValueRecursive(int rodSize, int* values)
+{
+	if (rodSize == 0)
+		return 0;
+	auto maxValue = INT_MIN;
+	for (auto i = 1; i <= rodSize/2; ++i)
+	{
+		//auto currentValue = 
+	}
+}
